b318fa0ac014c93f8a3a55233629dba40f983045
Adam Kelly <adam@cthulahoops.org>
2025-09-27 22:17:27 +0100

    Refactor: Move QueryClientProvider to App component to wrap everything.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { Router } from "./Router";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
     <div className="App">
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </div>
   );
 }
